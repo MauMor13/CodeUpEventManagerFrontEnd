abcaf0c30377d5eda29a42f118110f6e5b3302c8
Mauricio <mauri.f.mores@gmail.com>
2024-10-14 14:28:48 -0300

    ADD: new image and components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Main = styled.main`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding-right: 2rem;
+    padding-left: 2rem;
+    align-items: center;
+`;
+
+const Home = () => {
+    return (
+        <Main>
+            
+        </Main>
+    );
+}
+
+export default Home;
