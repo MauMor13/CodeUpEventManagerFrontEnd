503a90c964d5b098323efc607d7cbd99fe753277
Mauricio <mauri.f.mores@gmail.com>
2024-10-14 14:29:30 -0300

    REFACTOR: change default content

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Loading from './components/pure/Loading';
+import Header from './components/pure/Header';
+import Footer from './components/pure/Footer';
+import Home from './pages/Home';
+import { Routes, Route, HashRouter } from "react-router-dom";
+import isPropValid from '@emotion/is-prop-valid';
+import { StyleSheetManager } from 'styled-components';
 
-function App() {
-  const [count, setCount] = useState(0)
+function AppLoding() {
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <StyleSheetManager shouldForwardProp={isPropValid}>
+      <HashRouter>
+        <div className='container_body'>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </div>
+      </HashRouter>
+    </StyleSheetManager>
   )
 }
 
+function App(){
+  return <AppLoding />;
+}
 export default App
